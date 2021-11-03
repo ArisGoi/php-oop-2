20d703c6b0be7195e5add2a573533477814329a5
ArisGoi <aris.goi97@gmail.com>
2021-11-03 15:33:24 +0100

    user @param

--- classes/user.php
+++ classes/user.php
@@ -7,10 +7,9 @@ class Product {
     private $discount;
 
     /**
-     * @param str $_name nome del prodotto
-     * @param str $_description descrizione del prodotto
-     * @param num_int $_stars min=1 max=5
-     * @param num $_price (0.00) not < 0.1
+     * @param str $_name nome dell'utente
+     * @param str $_surname cognome dell'utente
+     * @param num_int $_eta età dell'utente
      */
     function __construct($_name, $_surname, $_eta)
     {
