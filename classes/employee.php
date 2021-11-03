e40354eaa8133ed7f6bef26df3b738e4fff23793
ArisGoi <aris.goi97@gmail.com>
2021-11-03 16:36:22 +0100

    add Employee

--- classes/employee.php
+++ classes/employee.php
@@ -1,5 +1,11 @@
 <?php
 require_once __DIR__ . "/user.php";
 class Employee extends User {
-    use User;
+
+    function __construct($_name, $_surname, $_eta, $_premium = false)
+    {
+        parent::__construct($_name, $_surname, $_eta, $_premium);
+
+        $this->user_discount = 0.90;
+    }
 }
